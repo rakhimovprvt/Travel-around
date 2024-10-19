16843a25ceaaf7bde05c2aa0fae13854e52b91ed
Abdulloh <R.abdulloh04@icloud.com>
2024-10-19 16:02:59 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+ <section>
+  <div class="bg-[url('/src/assets/images/Heroimagebg.png')] bg-cover">
+
+    <div class="px-[50px] py-[100px]">
+      <h1 class="font-semibold text-[72px] leading-[86px] text-white max-w-[581px] w-full mb-[40px]">Travel around Follow your heart</h1>
+      <p class="font-normal text-[24px] mb-[48px] leading-[32px] text-white max-w-[580px] w-full">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum. Quam vel aliquam sit vulputate. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum. Quam vel aliquam sit vulputate. </p>
+      <button class="bg-white text-black px-[56px] py-[14px] rounded-3xl mb-[45px]">Get started</button>
+    </div>
+  </div>
+  <div class="flex justify-center mb-[15px] relative top-[-5rem]">
+    <img class="rounded-xl" src="../public/Frame 14366.png" alt="">
+    <img class="rounded-xl" src="../public/Frame 14367.png" alt="">
+    <img class="rounded-xl" src="../public/Frame 14368.png" alt="">
+    <img class="rounded-xl" src="../public/Frame 14369.png" alt="">
+    <img class="rounded-xl" src="../public/Frame 14370.png" alt="">
+    <img class="rounded-xl" src="../public/Frame 14371.png" alt="">
+  </div>
+ </section>
+</template>
